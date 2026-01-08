9828801e5f53954c0ea7163c20e50c8ade16032f
Jackfar Sanidad <jaafarsanidad53@gmail.com>
2026-01-08 15:19:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -75,40 +75,40 @@ setInterval(() => {
 // --- SECTION 3: DATABASE ---
 const cityData = {
     "Cebu City": [
-        { title: "Cebu Ocean Park Ticket", price: "US$ 11.32", rating: "4.8", img: "/Picture/Cebu Ocean Park.webp" },
-        { title: "Oslob Whaleshark & Canyoneering", price: "US$ 65.65", rating: "4.6", img: "/Picture/Cebu Ocean Park.webp" },
-        { title: "Cebu Private Day Tour", price: "US$ 27.05", rating: "4.6", img: "/Picture/Cebu City Private Day Tour.webp" },
-        { title: "Plantation Bay Day Use", price: "US$ 43.05", rating: "4.7", img: "/Picture/Plantation Bay Day.webp" },
+        { title: "Cebu Ocean Park Ticket", price: "US$ 11.32", rating: "4.8", img: "Picture/Cebu Ocean Park.webp" },
+        { title: "Oslob Whaleshark & Canyoneering", price: "US$ 65.65", rating: "4.6", img: "Picture/Cebu Ocean Park.webp" },
+        { title: "Cebu Private Day Tour", price: "US$ 27.05", rating: "4.6", img: "Picture/Cebu City Private Day Tour.webp" },
+        { title: "Plantation Bay Day Use", price: "US$ 43.05", rating: "4.7", img: "Picture/Plantation Bay Day.webp" },
     ],
     "Manila": [
-        { title: "Manila Ocean Park Attraction", price: "US$ 17.95", rating: "4.7", img: "/Picture/Manila Ocean Park.png" },
-        { title: "Intramuros Bambike Tour", price: "US$ 19.55", rating: "4.8", img: "/Picture/Intramuros.webp" },
-        { title: "Fort Santiago Ticket", price: "US$ 1.35", rating: "4.8", img: "/Picture/Fort Santiago.jpg" },
-        { title: "Okada Manila Tour and Dine", price: "US$ 21.89", rating: "4.8", img: "/Picture/Okada Manila.webp" },
+        { title: "Manila Ocean Park Attraction", price: "US$ 17.95", rating: "4.7", img: "Picture/Manila Ocean Park.png" },
+        { title: "Intramuros Bambike Tour", price: "US$ 19.55", rating: "4.8", img: "Picture/Intramuros.webp" },
+        { title: "Fort Santiago Ticket", price: "US$ 1.35", rating: "4.8", img: "Picture/Fort Santiago.jpg" },
+        { title: "Okada Manila Tour and Dine", price: "US$ 21.89", rating: "4.8", img: "Picture/Okada Manila.webp" },
     ],
     "Baguio": [
-        { title: "Sky Ranch Baguio Day Pass", price: "US$ 14.85", rating: "4.5", img: "/Picture/Sky Ranch Baguio.webp" },
-        { title: "Atok Gardens Day Tour", price: "US$ 37.99", rating: "4.9", img: "/Picture/Atok Gardens.webp" },
-        { title: "Breathe Baguio Join In Tour", price: "US$ 35.99", rating: "4.7", img: "/Picture/Breathe Baguio.webp" },
-        { title: "Mt. Ulap Hiking Day Tour", price: "US$ 33.05", rating: "5.0", img: "/Picture/Mt. Ulap Hiking Day Tour from Baguio.webp" },
+        { title: "Sky Ranch Baguio Day Pass", price: "US$ 14.85", rating: "4.5", img: "Picture/Sky Ranch Baguio.webp" },
+        { title: "Atok Gardens Day Tour", price: "US$ 37.99", rating: "4.9", img: "Picture/Atok Gardens.webp" },
+        { title: "Breathe Baguio Join In Tour", price: "US$ 35.99", rating: "4.7", img: "Picture/Breathe Baguio.webp" },
+        { title: "Mt. Ulap Hiking Day Tour", price: "US$ 33.05", rating: "5.0", img: "Picture/Mt. Ulap Hiking Day Tour from Baguio.webp" },
     ],
     "Davao City": [
-        { title: "Davao City Tour", price: "US$ 22.39", rating: "4.7", img: "/Picture/Davao City Tour.webp" },
-        { title: "Malagos Garden Resort Pass", price: "US$ 9.55", rating: "4.8", img: "/Picture/Malagos Garden Resort.webp" },
-        { title: "Nature Tour in Davao", price: "US$ 43.05", rating: "4.8", img: "/Picture/Nature Tour in Davao.webp" },
-        { title: "Highlands Tour in Davao", price: "US$ 34.59", rating: "4.8", img: "/Picture/Highlands Tour in Davao.webp" },
+        { title: "Davao City Tour", price: "US$ 22.39", rating: "4.7", img: "Picture/Davao City Tour.webp" },
+        { title: "Malagos Garden Resort Pass", price: "US$ 9.55", rating: "4.8", img: "Picture/Malagos Garden Resort.webp" },
+        { title: "Nature Tour in Davao", price: "US$ 43.05", rating: "4.8", img: "Picture/Nature Tour in Davao.webp" },
+        { title: "Highlands Tour in Davao", price: "US$ 34.59", rating: "4.8", img: "Picture/Highlands Tour in Davao.webp" },
     ],
     "Puerto Princesa": [
-        { title: "Underground River Tour", price: "US$ 37.15", rating: "4.7", img: "/Picture/Puerto Princesa Underground River.webp" },
-        { title: "Honda Bay Island Hopping", price: "US$ 30.15", rating: "4.6", img: "/Picture/Honda Bay Palawan Island.webp" },
-        { title: "City Heritage Tour", price: "US$ 13.50", rating: "4.4", img: "/Picture/Puerto Princesa City Heritage Tour.webp" },
-        { title: "Sunset Watching Tour", price: "US$ 31.89", rating: "5.0", img: "/Picture/Sunset Watching.webp" },
+        { title: "Underground River Tour", price: "US$ 37.15", rating: "4.7", img: "Picture/Puerto Princesa Underground River.webp" },
+        { title: "Honda Bay Island Hopping", price: "US$ 30.15", rating: "4.6", img: "Picture/Honda Bay Palawan Island.webp" },
+        { title: "City Heritage Tour", price: "US$ 13.50", rating: "4.4", img: "Picture/Puerto Princesa City Heritage Tour.webp" },
+        { title: "Sunset Watching Tour", price: "US$ 31.89", rating: "5.0", img: "Picture/Sunset Watching.webp" },
     ],
     "Iloilo": [
-        { title: "Science XPdition Ticket", price: "US$ 10.10", rating: "4.5", img: "/Picture/Science XPdition.webp" },
-        { title: "Guimaras Island Day Tour", price: "US$ 51.45", rating: "4.8", img: "/Picture/Guimaras Island.webp" },
-        { title: "Gigantes & Sicogon Full-Day", price: "US$ 25.29", rating: "4.2", img: "/Picture/Gigantes & Sicogon Island.webp" },
-        { title: "Iloilo Pilgrimage Tour", price: "US$ 21.55", rating: "4.8", img: "/Picture/Iloilo Pilgrimage.webp" },
+        { title: "Science XPdition Ticket", price: "US$ 10.10", rating: "4.5", img: "Picture/Science XPdition.webp" },
+        { title: "Guimaras Island Day Tour", price: "US$ 51.45", rating: "4.8", img: "Picture/Guimaras Island.webp" },
+        { title: "Gigantes & Sicogon Full-Day", price: "US$ 25.29", rating: "4.2", img: "Picture/Gigantes & Sicogon Island.webp" },
+        { title: "Iloilo Pilgrimage Tour", price: "US$ 21.55", rating: "4.8", img: "Picture/Iloilo Pilgrimage.webp" },
     ]
 };
 
@@ -327,4 +327,5 @@ document.getElementById('inquiry-form').addEventListener('submit', function(e) {
     // Hide the inputs and show the success message
     document.getElementById('modal-form-content').classList.add('hidden');
     document.getElementById('modal-success-content').classList.remove('hidden');
+
 });
